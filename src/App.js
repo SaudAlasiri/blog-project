502677fd8c8c6903ab3556c79142af722b29df05
SaudAlasiri <156171816+SaudAlasiri@users.noreply.github.com>
2024-02-28 20:24:56 +0000

    Creating Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bu 3zoz v0.1</h1>
+      <HomePage />
     </div>
   );
 }
