032d4fcad57228c1044a85a7c98d7c8a1d38281c
SaudAlasiri <156171816+SaudAlasiri@users.noreply.github.com>
2024-02-29 08:38:34 +0000

    Responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+
+
 import { images } from '../constants'
 
 
 const navItemsInfo = [
-    {name: "Home"},
-    {name: "Articles"},
-    {name: "Pages"},
-    {name: "Pricing"},
-    {name: "Faq"},
+    { name: "Home" },
+    { name: "Articles" },
+    { name: "Pages" },
+    { name: "Pricing" },
+    { name: "Faq" },
 
 
 ]
 
-const NavItem = ({name}) => {
+const NavItem = ({ name }) => {
     return (
         <li className='relative group'>
             <a href="/" className='px-4 py-2'>{name}</a>
@@ -22,24 +26,39 @@ const NavItem = ({name}) => {
 }
 
 const Header = () => {
-  return (
-    <section>
-        <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
-            <div>
-              <img src={images.Logo} alt="logo" />
-            </div>
-            <div className='flex gap-x-9 items-center'>
-                <ul className='flex gap-x-2 font-semibold '>
-                    {navItemsInfo.map((item) => (
-                        <NavItem key={item.name} name={item.name} />
-                    ))}
-                    
-                </ul>
-                <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semiblod hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
-            </div>
-        </header>
-    </section>
-  )
+
+    const [navIsVisible, setNavIsVisible] = useState(false);
+
+    const navVisibilityHandler = () => {
+        setNavIsVisible((curSate) => {
+            return !curSate;
+        })
+
+    }
+
+
+
+    return (
+        <section>
+            <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
+                <div>
+                    <img className='w-16' src={images.Logo} alt="logo" />
+                </div>
+                <div className='z-50'>
+                    {navIsVisible ? <AiOutlineClose className='w-6 h-6' onClick={navVisibilityHandler} /> : <AiOutlineMenu className='w-6 h-6' onClick={navVisibilityHandler} />}
+                </div>
+                <div className={`${navIsVisible ? "right-0" : "-right-full"} z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0  lg:static gap-x-9 items-center`}>
+                    <ul className='flex gap-x-2 font-semibold '>
+                        {navItemsInfo.map((item) => (
+                            <NavItem key={item.name} name={item.name} />
+                        ))}
+
+                    </ul>
+                    <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semiblod hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
+                </div>
+            </header>
+        </section>
+    )
 }
 
 export default Header
