c0f873d9c9e373e47b8f017969921535ed55e472
SaudAlasiri <156171816+SaudAlasiri@users.noreply.github.com>
2024-03-11 08:38:39 +0000

    Articles card and component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,12 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from '../assets/HeroImage.svg';
+import post1 from "../assets/post1.jpg"
 
 const images = {
     Logo,
     HeroImage,
+    post1,
+
 };
 
 export default images;
