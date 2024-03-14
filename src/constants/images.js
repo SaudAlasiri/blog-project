b19f9b1b0cdfc2d6525686f3ffba00b1c51ac6ec
SaudAlasiri <156171816+SaudAlasiri@users.noreply.github.com>
2024-03-14 07:31:39 +0000

    ARTICLES AND COMPONENT

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,13 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from '../assets/HeroImage.svg';
 import post1 from "../assets/post1.jpg"
+import PostProfileImage from "../assets/post-profile.svg"
 
 const images = {
     Logo,
     HeroImage,
     post1,
+    PostProfileImage,
 
 };
 
